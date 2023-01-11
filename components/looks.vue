b1fe326b67e03126f25c2687945af15f6569382b
bh39dyby <bh39dyby@uni-leipzig.de>
2023-01-11 14:14:30 +0100

    looks desktop now max-w-7xl

--- components/looks.vue
+++ components/looks.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts"></script>
 <template>
-    <div class="flex flex-col items-center justify-center p-2 pt-10 lg:p-8">
-        <h2>{{ $t("p9.1") }}</h2>
-        <div class="h-full overflow-hidden">
-            <img class="hidden w-full lg:block" src="~/assets/IndieZoneScreenshot.png" alt="IndieZone.de Website Preview" />
-            <img class="w-full lg:hidden" src="~assets/IndieZoneScreenshotMobile.png" alt="IndieZone.de Website Preview" />
-        </div>
+  <div class="flex flex-col items-center justify-center p-2 pt-10 lg:p-8">
+    <h2>{{ $t("p9.1") }}</h2>
+    <div class="h-full overflow-hidden">
+      <img
+        class="hidden w-full max-w-7xl lg:block"
+        src="~/assets/IndieZoneScreenshot.png"
+        alt="IndieZone.de Website Preview"
+      />
+      <img
+        class="w-full lg:hidden"
+        src="~assets/IndieZoneScreenshotMobile.png"
+        alt="IndieZone.de Website Preview"
+      />
     </div>
+  </div>
 </template>
