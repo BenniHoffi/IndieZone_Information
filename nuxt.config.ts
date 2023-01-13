a65c13efe54c8e0e3d498997d54f008a4895db4d
bh39dyby <bh39dyby@uni-leipzig.de>
2023-01-13 14:17:21 +0100

    next try pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,9 +17,9 @@ export default defineNuxtConfig({
         "@nuxtjs/i18n",
         "@nuxtjs/supabase",
     ],
-    // nitro: {
-    //     preset: "cloudflare-pages",
-    // },
+    nitro: {
+        preset: "cloudflare-pages",
+    },
     i18n: {
         locales: [
             { code: "en", iso: "en-US", file: "locales/en.json", dir: "ltr" },
