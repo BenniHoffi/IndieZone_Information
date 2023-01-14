93851c6bbdc080510b9312bc9a56d2c5ac7821d5
bh39dyby <bh39dyby@uni-leipzig.de>
2023-01-14 17:36:02 +0100

    defaultsite gleiche wie homepage workaround

---
+++ pages/defaultsite.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+definePageMeta({
+  colorMode: "light",
+});
+
+useHead({
+  link: [{ rel: "canonical", href: "https://indiezone.de/" }],
+});
+</script>
+<template>
+  <div class="w-full h-screen overflow-x-hidden overflow-y-scroll snap-mandatory snap-y">
+    <SearchOnline class="w-full h-screen snap-center" />
+    <hr />
+    <Mark class="w-full h-screen snap-center" />
+    <hr />
+    <MarksProblem class="w-full h-screen snap-center" />
+    <hr />
+    <MarkWants class="w-full h-screen snap-center" />
+    <hr />
+    <Solution1 class="w-full h-screen snap-center" />
+    <hr />
+    <Solution2 class="w-full h-screen snap-center" />
+    <hr />
+    <MarkHappy class="w-full h-screen snap-center" />
+    <hr />
+    <ShopownerHappy class="w-full h-screen snap-center" />
+    <hr />
+    <Looks class="w-full h-screen snap-center" />
+    <hr />
+    <WhoWeAre class="w-full h-screen snap-center" />
+    <hr />
+    <Support class="w-full h-screen snap-center" />
+    <hr />
+    <Newsletter class="w-full h-screen snap-center" />
+  </div>
+</template>
